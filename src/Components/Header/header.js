317062356b69c211246f08eaa26a539e06436cb5
3omar3alaa <omaralaa1996@gmail.com>
2022-05-22 17:56:44 +0200

    Update Header Style

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -6,15 +6,22 @@ import { Link } from 'react-router-dom';
 const Header = (props) => {
 	return (
 		<div className='row header d-flex align-items-center mb-4'>
-			<div className='col'>
-				<h1>{props.header}</h1>
-			</div>
-			<div className='col fav'>
-				<Link to={`/favourites`}>
-					<h3>Favourites</h3>
-				</Link>
-			</div>
-			<MovieSearch searchValue={props.searchValue} setSearchValue={props.setSearchValue} />
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container">
+					<a class="navbar-brand" href="/">{props.header}</a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarResponsive">
+						<ul class="navbar-nav ms-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="/favourites">Favourites</a>
+							</li>
+						</ul>
+						<MovieSearch searchValue={props.searchValue} setSearchValue={props.setSearchValue} />
+					</div>
+				</div>
+			</nav>
 		</div>
 
 	);

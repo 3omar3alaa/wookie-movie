1d8913aa084777203a66aafba63259c8a29501f6
3omar3alaa <omaralaa1996@gmail.com>
2022-05-22 10:17:46 +0200

    Edit Movie Details Page

--- src/Components/Detail/detail.js
+++ src/Components/Detail/detail.js
@@ -1,34 +1,43 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { makeStyles, Grid, Button, CircularProgress } from '@material-ui/core';
+import Moment from 'moment';
+import Header from '../Header/header';
+import './detail.css';
 
 const MovieDetail = (props) => {
     const location = useLocation();
     //const { from } = location.state;
     // console.log(location.state);
     const movie = location.state;
-    if(movie === null){
+    if (movie === null) {
         console.log("here");
     }
     return (
-        // <>
-        //     {props.movie.map((movie, index) => (
-        //         <div className="image-container d-flex justify-content-start m-3">
-        //             <img src={movie.poster} alt="movie"></img>
-        //         </div>
-        //     ))}
-        // </>
-
-        <div>
-            <p>{movie.title}</p>
-              <p>Rating: {movie.imdb_rating}</p>
-              {/* <p>Release: {Moment(movie.released_on).format('DD-MM-YYYY')}</p> */}
-              <p>Classification: {movie.classification}</p>
-              <p>Cast: {movie.cast.map((cast, i) => {
-                if (i) { return `, ${cast}` }
-                return cast
-              })}</p>
-              <p>{movie.overview}</p>
-        </div>
+        <>
+            <Header header='Wookie Movies' />
+            <div className="backdrop" style={{ backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.75) 0%, rgba(0,0,0,0.6) 100%), url(${movie.backdrop})` }}>
+                <Grid container className="detail" spacing={3} >
+                    <Grid item xs={4} className="poster">
+                        <img src={movie.poster} alt={movie.title} />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <p className="title">{movie.title}</p>
+                        <p>Rating: {movie.imdb_rating}</p>
+                        <p>Release: {Moment(movie.released_on).format('DD-MM-YYYY')}</p>
+                        <p>Classification: {movie.classification}</p>
+                        <p>Cast: {movie.cast.map((cast, i) => {
+                            if (i) { return `, ${cast}` }
+                            return cast
+                        })}</p>
+                        <p className="overview">{movie.overview}</p>
+                    </Grid>
+                    <div className="flex justify-center mt-40">
+                        <Button variant="contained" color="primary">Add To Favourites</Button>
+                    </div>
+                </Grid>
+            </div>
+        </>
     );
 }
 
